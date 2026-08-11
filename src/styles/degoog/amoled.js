df1495c07244892d64bd613b2a9d8d3c24f51a86
itsmartashub <44645238+itsmartashub@users.noreply.github.com>
2026-08-11 21:07:32 +0200

    feat(degoog.org): Add `AMOLED` filter

---
+++ src/styles/degoog/amoled.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'Degoog.org - AMOLED',
+
+  domains: [
+    'degoog.org',
+  ],
+
+  css: `
+    [data-theme=dark] {
+        --bg: #000000 !important;
+        --bg-light: #121212 !important;
+        --search-bar-bg: #121212 !important;
+        --btn-bg: #131313 !important;
+        --search-bar-bg-hover: #141414 !important;
+    }
+  `,
+}
